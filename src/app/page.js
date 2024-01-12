05ecb84eabfe672ff9b973fc7c158106dc9ea02f
Isha <ishushree19@gmail.com>
2024-01-12 23:27:04 +0530

    added getparticipants function

--- src/app/page.js
+++ src/app/page.js
@@ -29,32 +29,49 @@ import React, { useEffect, useState } from 'react';
 
 const Home = () => {
   const [teamStats, setTeamStats] = useState(null);
+  const [totalParticipants, setTotalParticipants] = useState(null);
 
   useEffect(() => {
-    // Fetch data from backend API endpoint
+    // Fetch data from backend API endpoint for team statistics
     const fetchData = async () => {
       try {
-        const response = await fetch('api/analytics/totalTeams'); //  API endpoint
+        const response = await fetch('api/analytics/getTeams'); //  API endpoint
         const data = await response.json();
+        console.log(data);       
         setTeamStats(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
+    // Fetch data from backend API endpoint for total participants
+    const fetchTotalParticipants = async () => {
+      try {
+        const response = await fetch('api/analytics/getParticipants'); // API endpoint
+        const data = await response.json();
+        console.log(data);
+        setTotalParticipants(data);
+      } catch (error) {
+        console.error('Error fetching total participants:', error);
+      }
+    };
+
     fetchData();
+    fetchTotalParticipants();
   }, []);
 
   return (
     <div>
       <h1>Team Statistics</h1>
-      {teamStats ? (
+      {teamStats && totalParticipants ? (
         <>
           <p>Total Teams Registered: {teamStats["Total Teams Registered"]}</p>
           <p>4-member Teams: {teamStats["4-member teams"]}</p>
           <p>3-member Teams: {teamStats["3-member teams"]}</p>
           <p>2-member Teams: {teamStats["2-member teams"]}</p>
           <p>1-member Teams: {teamStats["1-member teams"]}</p>
+          <h1>Total Participants</h1>
+          <p>Total Number of Participants: {totalParticipants["totalParticipants"]}</p>
         </>
       ) : (
         <p>Loading team statistics...</p>
